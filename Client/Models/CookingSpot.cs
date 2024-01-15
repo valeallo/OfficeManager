1801530a9055040664ce37b6df5f673bf44d4840
valeallo <valentina.allodi@gmail.com>
2024-01-15 17:36:45 +0100

    created event that makes processorders run when a cooking spot is freed

--- Client/Models/CookingSpot.cs
+++ Client/Models/CookingSpot.cs
@@ -11,21 +11,32 @@ namespace Client.Models
         public FoodItem CurrentOrder { get; private set; }
         public bool IsOccupied => CurrentOrder != null;
         public DateTime OrderStartTime { get; private set; }
+        public event Action OnCookingSpotFreed;
 
         public void AssignOrder(FoodItem order)
         {
             CurrentOrder = order;
             OrderStartTime = DateTime.Now;
+            Task.Delay(TimeSpan.FromMinutes(order.PreparationTime)).ContinueWith(_ =>
+            {
+                CurrentOrder = null; 
+                OnCookingSpotFreed?.Invoke(); 
+            });
         }
 
         public bool CheckIfOrderIsCompleted()
         {
-            if (CurrentOrder == null) return false;
+            if (CurrentOrder == null)
+            {
+                OnCookingSpotFreed?.Invoke(); 
+                return true;
+            }
 
             var orderDuration = DateTime.Now - OrderStartTime;
             if (orderDuration.TotalMinutes >= CurrentOrder.PreparationTime)
             {
                 CurrentOrder = null;
+                OnCookingSpotFreed?.Invoke(); 
                 return true;
             }
             return false;
--- Client/Models/Restaurant.cs
+++ Client/Models/Restaurant.cs
@@ -11,6 +11,8 @@ namespace Client.Models
         public string Name { get; set; }
         public List<CookingSpot> CookingSpots { get; set; }
         public List<Menu> Menus { get; set; }
+        private List<Order> _allOrders;
+        private Queue<Order> _orderQueue;
 
         public Restaurant(string name)
         {
@@ -23,6 +25,15 @@ namespace Client.Models
             new CookingSpot()
         };
             Menus = new List<Menu>();
+            _allOrders = new List<Order>();
+            _orderQueue = new Queue<Order>();
+
+
+
+            foreach (var spot in CookingSpots)
+            {
+                spot.OnCookingSpotFreed += ProcessOrders; 
+            }
         }
 
         public void AddMenu(Menu menu)
@@ -35,5 +46,29 @@ namespace Client.Models
             return CookingSpots.Count(spot => !spot.IsOccupied);
         }
 
+        public void AddOrder(Order order)
+        {
+            _allOrders.Add(order);
+            _orderQueue.Enqueue(order);
+
+            ProcessOrders();
+        }
+
+
+        public void ProcessOrders()
+        {
+            foreach (var spot in CookingSpots)
+            {
+                if (!spot.IsOccupied && _orderQueue.Count > 0)
+                {
+                    var order = _orderQueue.Dequeue();
+                    foreach (var foodItem in order.FoodItems)
+                    {
+                        spot.AssignOrder(foodItem);
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
